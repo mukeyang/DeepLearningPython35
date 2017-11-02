875b7edbeac6df105b5ab12ac76361f135532f08
yang <634228743@qq.com>
2017-11-02 10:28:47 +0800

    add simple deepLearning demo

--- network2.py
+++ network2.py
@@ -334,7 +334,8 @@ def load(filename):
     f = open(filename, "r")
     data = json.load(f)
     f.close()
-    cost = getattr(sys.modules[__name__], data["cost"])
+    cost = getattr(
+        [__name__], data["cost"])
     net = Network(data["sizes"], cost=cost)
     net.weights = [np.array(w) for w in data["weights"]]
     net.biases = [np.array(b) for b in data["biases"]]
